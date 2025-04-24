fac935003817c09779f2c19665ab447a6242e7f1
MariaDaSilvaSerafim <mariadasilvaserafim42@gmail.com>
2025-04-24 11:08:14 -0300

    registro.py

--- registro.py
+++ registro.py
@@ -7,6 +7,8 @@ def main(page: Page):
     page.bgcolor = "#ffffff"
     page.padding = 0
 
+    lgn = Text("Sign In", color = "#000000", size = 40, weight = FontWeight.W_900)
+    lgn2 = Text("Ou se não tem cadastro, fazer cadastro agora!", color = "#000000")
     email = TextField(label = "Email", prefix_icon = icons.EMAIL)
     password = TextField(label = "Senha", prefix_icon = icons.PASSWORD, can_reveal_password = True, password = True)
     button1 = FilledButton(text = "Entrar", on_click = lambda e: entrar(e))
@@ -16,30 +18,27 @@ def main(page: Page):
 
         Container(height = 490, width = 385, bgcolor = "#000000",
                   border_radius = border_radius.only(top_right = 150, bottom_right = 150),
-                  content = Row([button2])
-                  ), 
-                
+
+                  content = Row([button2
+                                 
+                                 ], alignment = MainAxisAlignment.CENTER, vertical_alignment = CrossAxisAlignment.CENTER)), 
 
         Container(height = 490, width = 385, bgcolor = "#ffffff",
-                  content = Row([])
+                  content = Column([lgn, lgn2, email, password], ))
+                                
                   
-                  )
-                
-    ])
+    ], vertical_alignment = CrossAxisAlignment.CENTER, alignment = MainAxisAlignment.CENTER)
 
     login = Row([
 
          Container(height = 490, width = 385, bgcolor = "#ffffff",
                   
-                  content = Row([])
-                  ), 
-                
+                  content = Row([])),
 
         Container(height = 490, width = 385, bgcolor = "#000000", border_radius = border_radius.only(top_left = 150, bottom_left= 150),
                   content = Row([button1])
                   
                   )
-
     ])
 
     def entrar(e):
