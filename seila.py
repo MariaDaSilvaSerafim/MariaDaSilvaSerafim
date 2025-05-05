4f7df8e0c1c61a9db7517ab8bbaa6fc808b0bf84
MariaDaSilvaSerafim <mariadasilvaserafim42@gmail.com>
2025-05-05 08:10:02 -0300

    seila.py

--- seila.py
+++ seila.py
@@ -11,12 +11,26 @@ def main(page: Page):
     page.window_resizable = False
     page.window_maximizable = False
 
+    def logg(e):
+        if email.value == "mariadasilvaserafim42@gmail.com" and password == "maria123":
+            page.go()
+        else:
+            page.snack_bar = SnackBar(Text("Login Inválido"))
+            page.snack_bar.open = True
+            page.update
+
+    def cadast(e):
+
+        login.visible = False
+        cadastro.visible = True
+        page.update()
+
     #Objetos de login da página de Login
     lgn = Text("Login", height = 150, color = "#000000", size = 60, weight = FontWeight.W_900)
     lgn2 = TextButton(text = "Ou se não tem cadastro, fazer cadastro agora!", style = ButtonStyle(color = "#000000"))
     email = TextField(label = "Email", prefix_icon = icons.EMAIL, width = 300, border_radius = 20)
     password = TextField(label = "Senha", prefix_icon = icons.PASSWORD, can_reveal_password = True, password = True, width = 300, border_radius = 20)
-    button2 = FilledButton(text = "Voltar", style = ButtonStyle(bgcolor = "#000000", color = "#ffffff"), on_click = lambda e: voltar(e))
+    button2 = CupertinoButton(text = "Voltar", color = "#000000", bgcolor = "#ffffff", on_click = logg)
     
     #Imagem de login
     img1 = Image(
@@ -36,13 +50,13 @@ def main(page: Page):
     cademail = TextField(label = "Cadastre seu email", prefix_icon = icons.EMAIL, width = 300, border_radius = 20)
     password1 = TextField(label = "Crie sua senha", prefix_icon = icons.PASSWORD, can_reveal_password = True, password = True, width = 300, border_radius = 20)
     password2 = TextField(label = "Confirme sua senha", prefix_icon = icons.PASSWORD, can_reveal_password = True, password = True, width = 300, border_radius = 20)
-    button1 = FilledButton(text = "Entrar", style = ButtonStyle(bgcolor = "#000000", color = "#ffffff"), on_click = lambda e: entrar(e))
+    button1 = CupertinoFilledButton(text = "Entrar", on_click = cadast)
     
     #Imagem de cadastro
     img2 = Image(
-        src = "sistema escolar\infor.png",
-        width = 100,
-        height = 100,
+        src = "sistema escolar\imglogin.png",
+        width = 150,
+        height = 150,
         fit = ImageFit.CONTAIN,
     )
 
@@ -57,9 +71,9 @@ def main(page: Page):
         Container(height = 490, width = 385, bgcolor = "#000000", 
                   border_radius = border_radius.only(top_left = 150, bottom_left= 150), padding = 100,
                   
-                  content = Row([img1, button1
+                  content = Column([img2, button1
                                  
-                                 ], alignment = MainAxisAlignment.CENTER, vertical_alignment = CrossAxisAlignment.END))
+                                 ], alignment = MainAxisAlignment.CENTER, horizontal_alignment = CrossAxisAlignment.CENTER))
 
     ], vertical_alignment = CrossAxisAlignment.CENTER, alignment = MainAxisAlignment.CENTER)
     
@@ -81,16 +95,7 @@ def main(page: Page):
     ], vertical_alignment = CrossAxisAlignment.CENTER, alignment = MainAxisAlignment.CENTER)
 
 
-    def entrar(e):
-        cadastro.visible = True
-        login.visible = False
-        page.update()
-
-    def voltar(e):
-
-        login.visible = True
-        cadastro.visible = False
-        page.update()
+   
     
     page.add(login, cadastro)
 app(target = main)
