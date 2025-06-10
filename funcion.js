4ebce24b560a62aa808c1af8280711eba9299e5a
MariaDaSilvaSerafim <mariadasilvaserafim42@gmail.com>
2025-06-10 11:47:39 -0300

    funcion.js

---
+++ funcion.js
@@ -0,0 +1,23 @@
+function insertToDisplay(data){
+    document.querySelector('.display').value += data
+}
+
+function allclean(){
+    document.querySelector('.display').value = ''
+}
+
+function clean(){
+    const display = document.querySelector('.display')
+    display.value = display.value.slice(0, -1)
+}
+
+function result(){
+    const display = document.querySelector('.display')
+    try{
+
+        
+
+    }catch{
+
+    }
+}
